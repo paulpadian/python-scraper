af82fe89f3f58bb84421ccc51ae5719d039153f6
Paul <pdpadian@gmail.com>
2020-09-23 11:19:25 -0600

    changed target

--- scrape.py
+++ scrape.py
@@ -6,7 +6,7 @@ while True:
     page = 'https://www.repfitness.com/bars-plates/olympic-plates/bumper-plates/rep-black-bumper-plates'
     res = requests.get(page)
     soup = BeautifulSoup(res.content, 'html.parser')
-    stock = soup.find_all('p', 'availability out-of-stock')
+    stock = soup.find_all('p', 'availability')
     print(stock)
 
     time.sleep(15 * 60)
